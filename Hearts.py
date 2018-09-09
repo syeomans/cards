a05b0debf265315fc4599aff0320f12563ba3770
Samuel Yeomans <samuelyeomans@gmail.com>
2018-09-08 20:22:42 -0400

    Tested legalMoves function

--- Hearts.py
+++ Hearts.py
@@ -1,18 +1,19 @@
 from cards import *
 
-# Create deck and shuffle
-deck = deck()
-deck.shuffle()
-
-leadingCard = None
-heartsBroken = False
-
 class player:
 	def __init__(self, cardList):
 		self.hand = hand(cardList)
 		self.tricks = []
 		self.numTricks = 0
 		self.score = 0
+		self.has2C = query2C()
+
+	def query2C(self):
+		thisHand = self.hand.getHand()
+		for card in thisHand:
+			if card.getName() == "2C":
+				return(True)
+		return(False)
 
 	def takeTrick(self, cardList):
 		self.numTricks += 1
@@ -20,42 +21,55 @@ class player:
 
 	def legalMoves(self):
 		
+		thisHand = self.hand.getHand()
 		legalMoves = []
 
 		# If this player is leading the trick
 		if leadingCard == None:
 			# if hearts have been broken, anything can be played
 			if heartsBroken:
-				legalMoves = self.hand
+				for card in thisHand:
+					legalMoves.append(card.getName())
 			# if hearts haven't been broken, any non-heart can be played
 			else:
-				for card in self.hand:
-					if card.getSuit != "H":
-						legalMoves.append(card)
+				for card in thisHand:
+					if card.getSuit() != "H":
+						legalMoves.append(card.getName())
 
 		# If the player is not leading the trick
 		else:
 
 			# Add all cards of the same type as the leading card
-			for card in self.hand:
+			for card in thisHand:
 				if card.getSuit() == leadingCard.getSuit():
-					legalMoves.append(card)
+					legalMoves.append(card.getName())
 
 			# If there are no cards of the same type, any card can be played
 			if legalMoves == []:
-				legalMoves = self.hand
+				for card in thisHand:
+					legalMoves.append(card.getName())
 
 		return(legalMoves)
+
+	def play(self, cardName):
+		return(self.hand.play(cardName))
+
+# Create deck and shuffle
+deck = deck()
+deck.shuffle()
 		
-
-
 # Create players
-player1 = player(deck.deal(13))
-player2 = player(deck.deal(13))
-player3 = player(deck.deal(13))
-player4 = player(deck.deal(13))
+players = [player(deck.deal(13)) for i in range(0,4)]
 
-player1.hand.printHand()
-player2.hand.printHand()
-player3.hand.printHand()
-player4.hand.printHand()
+# Initialize global variables
+leadingCard = None
+heartsBroken = False
+
+
+
+
+
+
+# Test script
+players[0].hand.printHand()
+print(players[0].legalMoves())
